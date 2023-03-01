e4b3050aac24a744c23bf655f6df5ab40499a474
Luis <migapi00@gmail.com>
2023-03-01 10:59:05 +0100

    Empezando laravel

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+        return view ('home');
+    }
+
+    public function blog()
+    {
+        // $posts = Post::get();
+        // $posts = Post::first();
+        // $posts = Post::find(25);
+
+        //dd($post);
+
+        $posts = Post::latest()->paginate();
+        return view('blog', ['posts' => $posts]);
+
+
+    }
+
+    public function post(Post $post)
+    {
+            return view('post', ['post' => $post]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+
+/*
+    Route::get      |   consultar
+    Route::post     |   guardar
+    Route::delete   |   eliminar
+    Route::put      |   actualizar
+
+*/
+
+// Route::get('/', [PageController::class, 'home']) ->name('home');
+
+// Route::get('blog', [PageController::class, 'blog']) ->name('blog');
+
+// Route::get('blog/{slug}', [PageController::class, 'blog']) ->name('post');
+
+
+Route::controller(PageController::class)->group(function() {
+    Route::get('/',             'home') ->name('home');
+    Route::get('blog',          'blog') ->name('blog');
+    Route::get('blog/{post:slug}',   'post') ->name('post');
+});
